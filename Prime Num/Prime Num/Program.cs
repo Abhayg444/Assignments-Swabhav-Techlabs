978eabd3b5cfe3dba3270b77faf504103553c191
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-06 21:18:56 +0530

    Prime Number

---
+++ Prime Num/Prime Num/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Prime_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+            if (IsPrime(num))
+                Console.WriteLine($"{num} is a Prime Number.");
+            else
+                Console.WriteLine($"{num} is NOT a Prime Number.");
+
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number == 2)
+                return true;
+            if (number % 2 == 0)
+                return false;
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
