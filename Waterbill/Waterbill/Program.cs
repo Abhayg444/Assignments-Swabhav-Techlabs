66d07bebd35754a0b7fe55cea8ebd1645390569e
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-06 21:39:45 +0530

    WaterBill

---
+++ Waterbill/Waterbill/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace Waterbill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter units Consumed: ");
+            int Num_Units = int.Parse(Console.ReadLine());
+            Biller(Num_Units);
+        }
+        static void Biller(int Num_Units)
+        {
+            const int Meter_Charge = 75;
+            const int NotGrt100 = 5;
+            const int NotGrt250 = 10;
+            int charge;
+            if (Num_Units <= 100)
+            {
+                charge = Num_Units * NotGrt100;
+                Console.WriteLine("Your total bill is: " + (charge + Meter_Charge));
+            }
+            else if (Num_Units <= 250)
+            {
+                charge = Num_Units * NotGrt250;
+                Console.WriteLine("Your total bill is: " + (charge + Meter_Charge));
+            }
+            else
+            {
+                charge = Num_Units * 20;
+                Console.WriteLine("Your total bill is: " + (charge + Meter_Charge));
+            }
+        }
+    }
+}
