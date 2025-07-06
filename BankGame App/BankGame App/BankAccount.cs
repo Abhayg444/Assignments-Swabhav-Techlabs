4f90e50835a1c6410c13d589a2b7d3facf16605a
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-06 22:06:14 +0530

    BankGame App

---
+++ BankGame App/BankGame App/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankGame_App
+{
+    internal class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public double Balance { get; private set; }
+
+        public BankAccount(string accountHolder, double initialBalance)
+        {
+            AccountHolder = accountHolder;
+            Balance = initialBalance;
+        }
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be positive.");
+            }
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds for withdrawal.");
+            }
+            Balance -= amount;
+        }
+        public double GetBalance() { return Balance; }
+    }
+}
---
+++ BankGame App/BankGame App/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace BankGame_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rounds = 1;
+            string operation;
+            BankAccount bankAccount1 = new BankAccount("Alice", rnd.Next(5000, 100001));
+            BankAccount bankAccount2 = new BankAccount("Bob", rnd.Next(5000, 100001));
+            Console.WriteLine("--------------Bank Game App---------------\n");
+            //To check the logic is correct, uncomment the lines below
+
+            //Console.WriteLine($"Account1: {bankAccount1.AccountHolder} with initial balance: {bankAccount1.GetBalance()}");
+            //Console.WriteLine($"Account2: {bankAccount2.AccountHolder} with initial balance: {bankAccount2.GetBalance()}\n");
+            while (rounds < 4)
+            {
+                Console.WriteLine($"Round {rounds}");
+                Console.Write("Account1 Withdraw or Deposit (w/d):");
+            again:
+                operation = Console.ReadLine()?.ToLower();
+                switch (operation)
+                {
+                    case "w":
+                        Console.Write("Enter amount to withdraw:");
+                        if (double.TryParse(Console.ReadLine(), out double withdrawAmount1))
+                        {
+                            bankAccount1.Withdraw(withdrawAmount1);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid amount.");
+                        }
+                        break;
+                    case "d":
+                        Console.Write("Enter amount to deposit:");
+                        if (double.TryParse(Console.ReadLine(), out double depositAmount1))
+                        {
+                            bankAccount1.Deposit(depositAmount1);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid amount.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Please enter 'w' for withdraw or 'd' for deposit.");
+                        goto again;
+                }
+
+                Console.Write("Account2 Withdraw or Deposit (w/d):");
+            again2:
+                operation = Console.ReadLine()?.ToLower();
+                switch (operation)
+                {
+                    case "w":
+                        Console.Write("Enter amount to withdraw:");
+                        if (double.TryParse(Console.ReadLine(), out double withdrawAmount2))
+                        {
+                            bankAccount2.Withdraw(withdrawAmount2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid amount.");
+                        }
+                        break;
+                    case "d":
+                        Console.Write("Enter amount to deposit:");
+                        if (double.TryParse(Console.ReadLine(), out double depositAmount2))
+                        {
+                            bankAccount2.Deposit(depositAmount2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid amount.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Please enter 'w' for withdraw or 'd' for deposit.");
+                        goto again2;
+                }
+                rounds++;
+            }
+            if (bankAccount1.GetBalance() > bankAccount2.GetBalance())
+            {
+                Console.WriteLine($"\nWinner is {bankAccount1.AccountHolder} with balance: {bankAccount1.GetBalance()}");
+            }
+            else if (bankAccount1.GetBalance() < bankAccount2.GetBalance())
+            {
+                Console.WriteLine($"\nWinner is {bankAccount2.AccountHolder} with balance: {bankAccount2.GetBalance()}");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+
+            }
+        }
+    }
+}
