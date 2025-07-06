ed6578effae3d38d5d9da48ed927b00217b48bc3
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-06 12:23:21 +0530

    BMI Calculator

---
+++ BMI Calculator/BMI Calculator/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; } // in meters
+        public double Weight { get; set; } // in kilograms
+        public Person(int id, int age, string name, double height, double weight)
+        {
+            Id = id;
+            Age = age;
+            Name = name;
+            Height = height;
+            Weight = weight;
+        }
+
+        public Person(int id, string name ,int age)
+        {
+            Id= id;
+            Name = name;
+            Age = age;
+            Height = 5; 
+            Weight = 50; 
+        }
+        public double CalculateBMI()
+        {
+            return Weight / (Height * Height);
+        }
+        public string GetBodyType()
+        {
+            double bmi = CalculateBMI();
+            if (bmi < 18.5)
+                return "Underweight";
+            else if (bmi < 24.9)
+                return "Normal weight";
+            else if (bmi < 29.9)
+                return "Overweight";
+            else
+                return "Obesity";
+        }
+    }
+}
---
+++ BMI Calculator/BMI Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace BMI_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int continueProgram = 1;
+            while (continueProgram==1)
+            {
+                Console.WriteLine("Welcome to the BMI Calculator!");
+                Console.WriteLine(" 1. Custom Height & Weight \n 2. Default Height & Weight");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your ID:");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your Name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter your Age:");
+                        int age = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your Height (in meter):");
+                        double height = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your Weight (in kilograms):");
+                        double weight = double.Parse(Console.ReadLine());
+                        Person person1 = new Person(id, age, name, height, weight);
+                        Console.WriteLine($"Your BMI is: {person1.CalculateBMI()}");
+                        Console.WriteLine($"Your Body Type is: {person1.GetBodyType()}");
+
+                        Console.WriteLine("Do you want to continue? (1.yes / 2.no)");
+                        continueProgram = int.Parse(Console.ReadLine());
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter your ID:");
+                        id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Enter your Age:");
+                        age = int.Parse(Console.ReadLine());
+                        Person person2 = new Person(id, name, age);
+                        Console.WriteLine($"Your BMI is: {person2.CalculateBMI()}");
+                        Console.WriteLine($"Your Body Type is: {person2.GetBodyType()}");
+
+                        Console.WriteLine("Do you want to continue? (1.yes / 2.no)");
+                        continueProgram = int.Parse(Console.ReadLine());
+
+                        break;
+
+                        default:
+                        Console.WriteLine("Invalid choice. Please select 1 or 2.");
+                        continueProgram = 1; 
+                        break;
+                }
+            
+            }
+        }
+    }
+}
