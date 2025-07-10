c910564ccfb0802e74ab9acdc825fc441ccbea32
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-10 22:33:48 +0530

    Palindrome String

---
+++ Palindrome String/Palindrome String/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Palindrome_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to check if it is a palindrome: ");
+            string input = Console.ReadLine();
+            Palindrome_String(input);
+        }
+        static void Palindrome_String(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("Input cannot be null or empty.");
+                return;
+            }
+            string input = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+            string reversed = new string(input.Reverse().ToArray());
+            if (input == reversed)
+            {
+                Console.WriteLine($"\n{str} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{str} is not a palindrome.");
+            }
+        }
+    }
+}
