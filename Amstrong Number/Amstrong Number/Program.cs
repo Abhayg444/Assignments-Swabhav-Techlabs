29aedd21fc03ad280e9a8f26c8dc149bfe7186f8
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-10 16:33:01 +0530

    Amstrong Number

---
+++ Amstrong Number/Amstrong Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Amstrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Amstrong Number Check");
+            Console.Write("Enter a number to check if it is an Amstrong number:");
+            int num = int.Parse(Console.ReadLine());
+            int temp = num;
+            int amg = 0;
+            while (num > 0)
+            {
+                int rem = num % 10;
+                amg = amg + (rem * rem * rem);
+                num = num / 10;
+            }
+            if (amg == temp)
+            {
+                Console.WriteLine("It is an Amstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("It is not an Amstrong Number");
+            }
+        }
+    }
+}
