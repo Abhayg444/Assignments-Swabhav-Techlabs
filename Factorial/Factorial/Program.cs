37dcd4a24f48b11174574b33c4637c1990d9cce4
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-10 16:37:53 +0530

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to calculate its factorial:");
+            int number = int.Parse(Console.ReadLine());
+            Factorial(number);
+        }
+
+        public static void Factorial(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else if (number == 0 || number == 1)
+            {
+                Console.WriteLine("Factorial of " + number + " is 1.");
+            }
+            else
+            {
+                long result = 1;
+                for (int i = 2; i <= number; i++)
+                {
+                    result *= i;
+                }
+                Console.WriteLine("Factorial of " + number + " is " + result + ".");
+            }
+        }
+    }
+}
