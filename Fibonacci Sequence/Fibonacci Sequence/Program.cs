f2a3de7969d45d35d072756ffe0065464799d379
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-10 16:42:38 +0530

    Fibonacci Sequence

---
+++ Fibonacci Sequence/Fibonacci Sequence/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Fibonacci_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 =0; int num2 =1;
+            Console.Write("Fibonacci Sequence upto 8 numbers: ");
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"{num1} ");
+                int nextNum = num1 + num2;
+                num1 = num2;
+                num2 = nextNum;
+            }
+
+        }
+    }
+}
