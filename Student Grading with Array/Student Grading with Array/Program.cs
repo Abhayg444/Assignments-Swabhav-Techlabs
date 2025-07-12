8fe0e2f4d023d1f64633d1cf85bcf2fe856be76c
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-12 10:23:53 +0530

    Student Grading with array

---
+++ Student Grading with Array/Student Grading with Array/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Student_Grading_with_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------STUDENT GRADING SYSTEM-----------");
+            Console.WriteLine("Enter the number of students:");
+            int numOfStudents = int.Parse(Console.ReadLine());
+            Student[] students = new Student[numOfStudents];
+            int seats = 0;
+            while (numOfStudents > 0)
+            {
+                Console.WriteLine("Enter Student Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Roll Number:");
+                int roll = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Marks for 5 Subjects (English, Maths, Science, Social Science, Marathi):");
+                int[] marksArray = new int[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    marksArray[i] = int.Parse(Console.ReadLine());
+                }
+                students[seats] = new Student(name, roll, marksArray);
+                seats++;
+                numOfStudents--;
+            }
+
+            /*Student student1 = new Student("Jatin Pandey", 1, new int[] { 85, 90, 78, 88, 92 });
+            Student student2 = new Student("Jasmine Shaikh", 2, new int[] { 75, 80, 70, 65, 60 });
+            Student student3 = new Student("Rohan Verma", 3, new int[] { 95, 88, 92, 85, 90 });
+            Student student4 = new Student("Aisha Khan", 4, new int[] { 60, 70, 65, 75, 80 });
+
+            student1.printReport(student1);
+            student2.printReport(student2);
+            student3.printReport(student3);
+            student4.printReport(student4);*/
+
+            foreach (var student in students)
+            {
+                student.printReport(student);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Student Grading with Array/Student Grading with Array/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grading_with_Array
+{
+    internal class Student
+    {
+        public string studentName;
+        public int rollNumber;
+        public int[] marks = new int[5];
+
+        public Student(string name, int roll, int[] marksArray)
+        {
+            studentName = name;
+            rollNumber = roll;
+            marks = marksArray;
+        }
+
+        public double CalculateAverage(Student student)
+        {
+            return student.marks.Average();
+        }
+
+        public string calculateGrade(double marks)
+        {
+            if (marks >= 90)
+            {
+                return "A+";
+            }
+            else if (marks >= 80)
+            {
+                return "A";
+            }
+            else if (marks >= 70)
+            {
+                return "B";
+            }
+            else if (marks >= 60)
+            {
+                return "C";
+            }
+            else if (marks >= 50)
+            {
+                return "D";
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
+
+        public void printReport(Student student)
+        {
+            Console.WriteLine("Student Name: " + student.studentName);
+            Console.WriteLine("Roll Number: " + student.rollNumber);
+            Console.WriteLine($"Marks : English: {marks[0]}, Maths: {marks[1]}, Science: {marks[2]}, Social Science: {marks[3]}, Marathi: {marks[4]}");
+            double average = CalculateAverage(student);
+            Console.WriteLine("Average Marks: " + average);
+            Console.WriteLine("Grade: " + calculateGrade(average));
+        }
+    }
+}
