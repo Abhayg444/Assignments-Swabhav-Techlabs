318b2be0d2981f4ec3b487624602033b22116f68
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 23:33:25 +0530

    Testing

---
+++ Test/ArrayTest/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArrayTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            names[0] = "Abhay";
+            names[1] = "Amit";
+            names[2] = "Ankit";
+            names[3] = "Anshul";
+            names[4] = "Ankush";
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine();    
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Test/Test/LoopTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class LoopTest
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Abhay";
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            Console.WriteLine();
+
+            while(name.Length > 0)
+            {
+                Console.WriteLine(name[0]);
+                name = name.Substring(1);
+            }
+        }
+    }
+}
