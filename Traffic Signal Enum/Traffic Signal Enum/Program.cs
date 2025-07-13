53248c933f6d77b329ccee4403bfc448c9e018ae
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 19:47:51 +0530

    Traffic Signal Enum

---
+++ Traffic Signal Enum/Traffic Signal Enum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Traffic_Signal_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Traffic Light \n 1.Red \n 2.Yellow\n 3.Green  :");
+            int signal= int.Parse(Console.ReadLine());
+            Traffic_Signal trafficSignal = new Traffic_Signal();
+            trafficSignal.signalInfo(signal);
+
+        }
+    }
+}
---
+++ Traffic Signal Enum/Traffic Signal Enum/Traffic Signal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Signal_Enum
+{
+    enum Traffic_Signal_Info
+    {
+        Red,
+        Yellow,
+        Green
+    }
+   
+    internal class Traffic_Signal
+    {
+        public void signalInfo(int signal)
+        {
+            switch (signal)
+            {
+                case 1:
+                    Console.WriteLine($"Signal: {Traffic_Signal_Info.Red}-->Stop");
+                    break;
+                case 2:
+                    Console.WriteLine($"Signal: {Traffic_Signal_Info.Yellow}-->Get Ready");
+                    break;
+                case 3:
+                    Console.WriteLine($"Signal:  {Traffic_Signal_Info.Green}-->Go");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Signal");
+                    break;
+            }
+        }
+    }
+}
