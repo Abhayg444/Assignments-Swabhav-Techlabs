d5e338aa98efbd582113629d8813768eac109deb
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 20:46:20 +0530

    Words Count

---
+++ Count Words/Count Words/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Count_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count Words in a Sentence");
+            Console.Write("Enter the Sentence: ");
+            string input = Console.ReadLine();
+
+            string[] Words = input.Split(" ").ToArray();
+            Console.WriteLine($"Number of Words: {Words.Length}");
+        }
+    }
+}
