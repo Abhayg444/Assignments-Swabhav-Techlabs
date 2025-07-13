ec90618b03865df230af868531c8ce68819020e2
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 12:46:43 +0530

    Count Spaces

---
+++ Count Spaces/Count Spaces/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Count_Spaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the sentence: ");
+            string sentence = Console.ReadLine();
+            string[] strings = sentence.Split(" ").ToArray();
+            int spaceCount = strings.Length-1;
+
+            Console.WriteLine($"\nNumber of spaces in the sentence : {spaceCount}");
+        }
+    }
+}
