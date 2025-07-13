e054b72077328c6a4cc9edf8b96ab151a6164be9
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 13:08:06 +0530

    POC on Enum

---
+++ POC on ENUM/POC on ENUM/Days of the Week.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POC_on_ENUM
+{
+    enum WorkingDaysofTheWeek
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday
+    }
+    internal class Days_of_the_Week
+    {
+        public string Day(WorkingDaysofTheWeek day)
+        {
+            switch (day)
+            {
+                case WorkingDaysofTheWeek.monday:
+                    return "\n  Monday is the first day of the week.\n  Today you have to prepare for presentation.";
+
+                case WorkingDaysofTheWeek.tuesday:
+                    return "\n  Tuesday is the second day of the week.\n  Today you have to prepare for meeting.";
+
+                case WorkingDaysofTheWeek.wednesday:
+                    return "\n  Wednesday is the third day of the week.\n  Today you have to prepare for report.";
+
+                case WorkingDaysofTheWeek.thursday:
+                    return "\n  Thursday is the fourth day of the week.\n  Today you have to prepare for review.";
+
+                case WorkingDaysofTheWeek.friday:
+                    return "\n  Friday is the fifth day of the week.\n  Today you have to prepare for weekend.";
+
+                default:
+                    return "\nInvalid day.";
+
+            }
+        }
+    }
+}
---
+++ POC on ENUM/POC on ENUM/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace POC_on_ENUM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Know your schedule for the day \n 1.Monday 2.Tuesday 3.Wednesday 4.Thursday 5.Friday : ");
+            int day = int.Parse(Console.ReadLine());
+            Days_of_the_Week workday = new Days_of_the_Week();
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine(workday.Day(WorkingDaysofTheWeek.monday));
+                    break;
+                case 2:
+                    Console.WriteLine(workday.Day(WorkingDaysofTheWeek.tuesday));
+                    break;
+                case 3:
+                    Console.WriteLine(workday.Day(WorkingDaysofTheWeek.wednesday));
+                    break;
+                case 4:
+                    Console.WriteLine(workday.Day(WorkingDaysofTheWeek.thursday));
+                    break;
+                case 5:
+                    Console.WriteLine(workday.Day(WorkingDaysofTheWeek.friday));
+                    break;
+                default:
+                    Console.WriteLine("Invalid day.");
+                    break;
+            }          
+        }
+    }
+}
