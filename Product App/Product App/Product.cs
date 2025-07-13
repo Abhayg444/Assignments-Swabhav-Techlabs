27555a8bd594041c4c1e56dba5135a389996403f
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 18:05:03 +0530

    Product App

---
+++ Product App/Product App/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_App
+{
+    internal class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+        public readonly double DiscountPercentage = 20;
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public decimal GetDiscountedPrice()
+        {
+            return Price - (Price * (decimal)(DiscountPercentage / 100));
+        }
+
+        public void DisplayProductInfo()
+        {
+            Console.WriteLine($"\nProduct ID: {Id}");
+            Console.WriteLine($"Product Name: {Name}");
+            Console.WriteLine($"Original Price: {Price} Rs");
+            Console.WriteLine($"Discounted Price: {GetDiscountedPrice()} Rs\n");
+        }
+    }
+}
