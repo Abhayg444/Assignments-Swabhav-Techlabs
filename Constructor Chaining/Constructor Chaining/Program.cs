94506c220c6f2d2f4abb01a745ab2a56aff1b251
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 13:36:17 +0530

    Constructor Chaining POC

---
+++ Constructor Chaining/Constructor Chaining/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Constructor_Chaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing Unboxing Example  
+            int num = 123456;
+            object boxedNum = num;
+
+            Console.WriteLine($"Boxed Value: {boxedNum}");
+
+            int unboxedNum = (int)boxedNum;
+            Console.WriteLine($"Unboxed Value: {unboxedNum}\n\n");
+
+            // Constructor Chaining Example
+
+            Student student1 = new Student(1, "Alice", "Mathematics", 20);
+            Student student2 = new Student(2, "Bob", "Physics");
+            Student student3 = new Student(3, "Charlie");
+
+            student1.Display();
+            student2.Display();
+            student3.Display();
+        }
+    }
+}
---
+++ Constructor Chaining/Constructor Chaining/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+        public string Course;
+        public int Age;
+
+        public Student(int id, string name, string course, int age)
+        {
+            Id = id;
+            Name = name;
+            Course = course;
+            Age = age;
+        }
+
+        public Student(int id, string name, string course)
+        {
+            Id = id;
+            Name = name;
+            Course = course;
+            Age = 18;
+
+        }
+
+        public Student(int id, string name) 
+        {
+            Id = id;
+            Name = name;
+            Course = "Default Course";
+            Age = 18;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}, Course: {Course}, Age: {Age}");
+        }
+    }
+}
