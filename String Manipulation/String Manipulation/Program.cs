e9ad1241e5eac2fb594ad825942a28d44e471848
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 12:40:43 +0530

    String Manipulation

---
+++ String Manipulation/String Manipulation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace String_Manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the String by Developer,Company,Location: ");
+            string details = Console.ReadLine();
+
+            string[] strings = details.Split(",").ToArray();
+            Console.WriteLine($"\nDeveloper Name is {strings[0]}");
+            Console.WriteLine($"Company Name is {strings[1]}");
+            Console.WriteLine($"Location is {strings[2]}");
+
+        }
+    }
+}
