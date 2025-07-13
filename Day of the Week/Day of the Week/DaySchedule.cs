642d8c5ba1d3a8815b99484bdc112e87cafcdb96
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 13:19:30 +0530

    Days of Week

---
+++ Day of the Week/Day of the Week/DaySchedule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_of_the_Week
+{
+    enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+    internal class DaySchedule
+    {
+        public Day Schedule(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    return Day.Monday;
+
+                case 2:
+                    return Day.Tuesday;
+
+                case 3:
+                    return Day.Wednesday;
+
+                case 4:
+                    return Day.Thursday;
+
+                case 5:
+                    return Day.Friday;
+
+                case 6:
+                    return Day.Saturday;
+
+                case 7:
+                    return Day.Sunday;
+
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Day of the Week/Day of the Week/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_of_the_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Days of the Week Enum (1-7 ): ");
+            DaySchedule daySchedule = new DaySchedule();
+            int dayNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Day is: {daySchedule.Schedule(dayNumber)}");
+
+        }
+    }
+}
