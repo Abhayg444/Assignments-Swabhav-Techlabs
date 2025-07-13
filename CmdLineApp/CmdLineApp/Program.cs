493fb27a1ca16057440245e1aa7326e705d5501d
Abhayg444 <abhaysgupta722@gmail.com>
2025-07-13 23:16:05 +0530

    CmdLine

---
+++ CmdLineApp/CmdLineApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CmdLineApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = args.Select(int.Parse).ToArray();
+
+            int count = numbers.Length;
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+            int min = numbers.Min();
+
+            Console.WriteLine($"Count: {count}");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average:F2}");
+            Console.WriteLine($"Maximum: {max}");
+            Console.WriteLine($"Minimum: {min}");
+        }
+
+    }
+}
